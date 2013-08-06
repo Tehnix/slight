532032a89a45c553674db0b048af8b66b67dfdce
Christian Kjær Laustsen <christianlaustsen@gmail.com>
2013-08-06 09:50:49 +0200

    Auto-gen settings, IRC bot and threading.
    Add auto-generated settings file with validation on it. Implement a basic IRC bot
    that will send the notifications out into the channels. Fix the threading of all
    the parts that needs to be threaded.

--- slight.py
+++ slight.py
@@ -1,12 +1,14 @@
 import sys
-import json
+import os
+import subprocess
+import threading
 import urllib2
 import BaseHTTPServer
 import SimpleHTTPServer
-import subprocess
-import threading
+import json
+import yaml
 
-#import irc_bot
+import irc_bot
 
 
 class MyHTTPServer(BaseHTTPServer.HTTPServer):
@@ -28,65 +30,136 @@ class MyHTTPServerHandler(BaseHTTPServer.BaseHTTPRequestHandler):
             json_payload = json.loads(urllib2.unquote(post_body[8:]))
             data = parse(json_payload)
             ident = '{0}/{1}'.format(data["name"], data["repo_name"])
-            thread = threading.Thread(target=handle_hook, args=(ident, data))
-            thread.start()
+            t = threading.Thread(target=handle_hook, args=(ident, data))
+            t.start()
 
-def handle_hook(ident, data):
-    # Only execute on push to master
-    sem = MyHTTPServerHandler.get_lock(ident)
-    sem.acquire()
-    if data is not None and data["refs"] == "refs/heads/master":
-        git_update(data)
-        execute_shell_script(data)
-    sem.release()
+def run_server():
+    httpd = MyHTTPServer(('', 13373), MyHTTPServerHandler)
+    httpd.serve_forever()
 
 def parse(json):
+    """Parse the received JSON and return a dict of the valuable information."""
     try:
         repo_name = json["repository"]["name"]
         owner_name = json["repository"]["owner"]["name"]
         push_name = json["pusher"]["name"]
-        commits = len(json["commits"])
+        commits = str(len(json["commits"]))
         refs = json["refs"]
+        url = json["repository"]["url"]
     except IndexError:
         return None
     else:
         return {
             "name": owner_name, "repo_name": repo_name,
             "push_name": push_name, "commits": commits,
-            "refs": refs
+            "refs": refs, "url": url
         }
 
+def handle_hook(ident, data):
+    """
+    Manage the different actions that need to be taken once a hook is received.
+    Semaphores are used to make sure two pushs in quick succession doesn't happen
+    simultaneously, but, rather sequentially.
+     
+    """
+    # Only execute on push to master
+    sem = MyHTTPServerHandler.get_lock(ident)
+    sem.acquire()
+    if data is not None and data["refs"] == "refs/heads/master":
+        check_repository_existence(data)
+        git_update(data)
+        execute_shell_script(data)
+    sem.release()
+
+def check_repository_existence(data):
+    """
+    Check if a repository exists (and the subsequent owner), else, clone
+    down the repository from the url received with the hook.
+    
+    """
+    owner_dir = 'repos/{0}'.format(data["name"])
+    if not os.path.isdir(owner_dir):
+        os.mkdir(owner_dir)
+    repo_dir = 'repos/{0}/{1}'.format(data["name"], data["repo_name"])
+    if not os.path.isdir(repo_dir):
+        notify('Cloning `{1}/{2}`...'.format(data["name"], data["repo_name"]))
+        subprocess.call(['git', 'clone', data["url"], repo_dir])
+    
 def git_update(data):
+    """Perform a git pull in the repository."""
     notify('Hook received by `{0}` for `{1}/{2}`, performing pull...'.format(
         data["push_name"], data["name"], data["repo_name"]
     ))
-    # perform git pull here
-    notify('!!!!!!!!!! --> git pull not implemented yet! <-- !!!!!!!!!!!!')
+    repo_dir = 'repos/{0}/{1}'.format(data["name"], data["repo_name"])
+    with open(os.devnull, 'w') as devnull:
+        subprocess.Popen(
+            ['git', 'pull', 'origin', 'master'],
+            cwd=repo_dir, stdout=devnull, stderr=devnull
+        )
     notify('Updated `{0}/{1}` with {2} commits!'.format(
         data["name"], data["repo_name"], data["commits"]
     ))
     
-
 def execute_shell_script(data):
-    script = './{0}_{1}'.format(data["name"].lower(), data["repo_name"].lower())
-    try:
-        notify('Starting job for `{0}/{1}`'.format(data["name"], data["repo_name"]))
-        subprocess.call([script])
-        notify('Done with job `{0}/{1}`!'.format(data["name"], data["repo_name"]))
-    except OSError, e:
-        if str(e).startswith('[Errno 2]'):
-            notify('No script to execute. Please create one first!')
-        elif str(e).startswith('[Errno 13]'):
-            notify('The script is not executable!')
-        elif str(e).startswith('[Errno 8]'):
-            notify('Format error with the script!')
-        else:
-            notify('Uncaught error, calling script: {0}'.format(str(e)))
+    """Execute the accompanying shell/deploy script for the repo."""
+    script = 'scripts/{0}_{1}'.format(data["name"].lower(), data["repo_name"].lower())
+    if os.path.exists(script):
+        try:
+            notify('Starting job for `{0}/{1}`'.format(data["name"], data["repo_name"]))
+            subprocess.call(['./{0}'.format(script)])
+            notify('Done with job `{0}/{1}`!'.format(data["name"], data["repo_name"]))
+        except OSError, e:
+            if str(e).startswith('[Errno 2]'):
+                notify('No script to execute. Please create one first!')
+            elif str(e).startswith('[Errno 13]'):
+                notify('The script is not executable!')
+            elif str(e).startswith('[Errno 8]'):
+                notify('Format error with the script!')
+            else:
+                notify('Uncaught error, calling script: {0}'.format(str(e)))
+    else:
+        notify('No script to execute. Please create one first!')
 
 def notify(msg):
-    print msg
+    irc_bot.add_to_queue(msg)
 
+def create_first_settings_file():
+    if not os.path.exists('slight.conf'):
+        with open('slight.conf', 'w') as conf:
+            conf.write("\n".join([
+                "# -- Example of usage --",
+                "#irc:",
+                "#    irc.freenode.net:",
+                "#        port: 6697",
+                "#        ssl: true",
+                "#        channel: \"#lobby\"",
+                "#        nickname: \"slight-ci\""
+            ]))
+
+def check_settings_validity():
+    with open('slight.conf', 'r') as conf:
+        settings = yaml.safe_load(conf)
+        irc = settings.get('irc')
+        if settings is not None and irc is not None:
+            for server, info in irc.items():
+                if info.get('port') is None or info.get('ssl') is None or info.get('channel') is None or info.get('nickname') is None:
+                    print "Invalid slight.conf file. nickname, port, ssl and channel must be set!"
+                    return False
+                return True
+    return None
+
+def fetch_irc_servers_from_settings():
+    with open('slight.conf', 'r') as conf:
+        settings = yaml.safe_load(conf)
+        return settings.get('irc')
 
 if __name__ == "__main__":
-    httpd = MyHTTPServer(('', 13373), MyHTTPServerHandler)
-    httpd.serve_forever()
+    create_first_settings_file()
+    check_settings = check_settings_validity()
+    if check_settings and check_settings is not None:
+        servers = fetch_irc_servers_from_settings()
+        irc_bot.start_irc_bot(servers)
+    if check_settings or check_settings is None:
+        t = threading.Thread(target=run_server)
+        t.start()
+        t.join()
